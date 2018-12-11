e8ca16c277b38c3f221f2320622c4663c34d828b
jag-ermeister <jgustine@pivotal.io>
2018-12-10 20:14:13 -0700

    Added Spring Boot 2 project

---
+++ logging-app/src/main/java/com/pivotal/logging/app/loggingapp/LoggingAppApplication.java
@@ -0,0 +1,12 @@
+package com.pivotal.logging.app.loggingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingAppApplication.class, args);
+	}
+}
