c19d6251b6c3290a7071fe0cda303ba0d8d3531f
jag-ermeister <jgustine@pivotal.io>
2019-01-09 22:07:35 -0700

    Added hadoop example

---
+++ hadoop/src/main/java/demo/Main.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    static Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Logging from slf4j logger!");
+    }
+}
---
+++ hadoop/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -0,0 +1,38 @@
+package org.slf4j.impl;
+
+import org.slf4j.ILoggerFactory;
+import org.slf4j.spi.LoggerFactoryBinder;
+
+/*
+Last answer here: https://stackoverflow.com/questions/11433926/force-slf4j-to-use-logback
+Rad's answer here is also helpful: https://stackoverflow.com/questions/17829995/how-does-slf4j-bind-to-implementation-does-it-really-do-so-during-compile-time
+Only one StaticLoggerBinder class is used. Since this is MY code, it is first in the classpath, and chosen first.
+*/
+
+
+/**
+ * Force use of Log4j binding for logging.
+ */
+@SuppressWarnings("UnusedDeclaration")
+public class StaticLoggerBinder implements LoggerFactoryBinder {
+    private static final StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
+
+    public static String REQUESTED_API_VERSION = "1.6";
+
+    public static final StaticLoggerBinder getSingleton() {
+        return SINGLETON;
+    }
+
+    private StaticLoggerBinder() {
+    }
+
+    @Override
+    public ILoggerFactory getLoggerFactory() {
+        return new Log4jLoggerFactory();
+    }
+
+    @Override
+    public String getLoggerFactoryClassStr() {
+        return "org.slf4j.impl.Log4jLoggerFactory";
+    }
+}
